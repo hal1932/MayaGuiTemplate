bb7c32fc4d0118f1459381e3979f815f1bbfc391
hal1932 <yu.arai.19@gmail.com>
2016-02-20 05:36:44 +0900

    initial commit

---
+++ MayaGuiTemplate/MayaGuiTemplatePlugin.cs
@@ -0,0 +1,40 @@
+﻿#if !VIEWDEBUG
+using Autodesk.Maya.OpenMaya;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: ExtensionPlugin(typeof(MayaGuiTemplate.TemplateCommandPlugin), "hal1932", "1.0", "Any")]
+[assembly: MPxCommandClass(typeof(MayaGuiTemplate.TemplateCommand), "template")]
+
+namespace MayaGuiTemplate
+{
+    public class TemplateCommand : MPxCommand, IMPxCommand
+    {
+        public override void doIt(MArgList args)
+        {
+            new MainWindow().Show();
+        }
+    }
+
+    public class TemplateCommandPlugin : IExtensionPlugin
+    {
+        public string GetMayaDotNetSdkBuildVersion()
+        {
+            return "201353";
+        }
+
+        public bool InitializePlugin()
+        {
+            return true;
+        }
+
+        public bool UninitializePlugin()
+        {
+            return true;
+        }
+    }
+}
+#endif // #if VIEWDEBUG
---
+++ ViewDebugEntryPoint/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MayaGuiTemplate;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ViewDebugEntryPoint
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+    }
+}
